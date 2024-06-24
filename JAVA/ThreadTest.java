548ddea1760cf5c35641db74dfa308f5328593fd
ghsalah <99658661+ghsalah@users.noreply.github.com>
2024-06-24 10:19:10 +0530

    Create ThreadTest.java

---
+++ JAVA/ThreadTest.java
@@ -0,0 +1,35 @@
+clara A estands Thread
+
+public veld zuni)
+
+fortint 101/ 10-52++ System.out.println("\tFrom Threail A
+
+System.out.println("Exit form A
+
+clase & extends Thread
+
+mihlic void run() for lint 10-5 ++
+
+System.out.println("\tFrom Thread B
+
+System.out.println("Exit from B");
+
+Class C extends Thread
+
+public void run()
+
+Zortint kel, kc-57 k++)
+
+System.out.println("\tFrom Thread Ck+k)
+
+System.out.println("Exit from C");
+
+class ThreadTest
+
+public static void main(String args[ 1)
+
+new Ai ).start()
+
+new Bi).start();
+
+new Cli.start():
